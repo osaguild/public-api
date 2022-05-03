ceff3de94a31adec5fd95b7d544f61f7f86852ea
osaguild <nobuhisa.abe.0922@gmail.com>
2022-05-03 14:55:50 +0900

    use taberogu uri

--- taberogu/taberogu.ts
+++ taberogu/taberogu.ts
@@ -1,11 +1,22 @@
 const axios = require('axios');
+const cheerio = require('cheerio');
 
 export async function get(event: any): Promise<any> {
-  let zipcode = '9071801';
-  const res = await axios.get(`https://api.zipaddress.net/?zipcode=${zipcode}`);
+  const area = "さいたま市";
+  const name = event.queryStringParameters.name;
+  //const name = "日高屋";
+  const uri = `https://tabelog.com/saitama/A1101/rstLst/?vs=1&sa=${area}&sw=${name}`;
+  const encodeUri = encodeURI(uri);
+  const res = await axios.get(encodeUri);
+  const $ = cheerio.load(res.data);
+  let shops: string[] = [];
+  $(".js-rst-cassette-wrap").each((index: Number, element: String) => {
+    shops.push($(element).attr("data-detail-url"));
+  });
+  
   return {
     statusCode: 200,
     headers: { "Access-Control-Allow-Origin": "*" },
-    body: res.data.data.pref,
+    body: shops[0],
   };
 }
