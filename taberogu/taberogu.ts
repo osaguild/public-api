63e444938e73d328de64a2cc4a4cc4e53828934a
osaguild <nobuhisa.abe.0922@gmail.com>
2022-05-04 16:00:19 +0900

    add star

--- taberogu/taberogu.ts
+++ taberogu/taberogu.ts
@@ -1,21 +1,37 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-export async function get(event: any): Promise<any> {
+export async function getShop(event: any): Promise<any> {
+  // create request
   const area = "さいたま市";
   const name = event.queryStringParameters.name;
-
   const uri = `https://tabelog.com/saitama/A1101/rstLst/?vs=1&sa=${area}&sw=${name}`;
-  const encodeUri = encodeURI(uri);
-  const res = await axios.get(encodeUri);
+  const encodedUri = encodeURI(uri);
+
+  // get dom
+  const res = await axios.get(encodedUri);
+  
+  // search dom
   const $ = cheerio.load(res.data);
-  let shopIds: string[] = [];
-  let shopUrls: string[] = [];
+  const shopIds: string[] = [];
+  const shopUrls: string[] = [];
+  const shopStars: Number[] = [];
   $(".js-rst-cassette-wrap").each((index: Number, element: String) => {
     shopIds.push($(element).attr("data-rst-id"));
     shopUrls.push($(element).attr("data-detail-url"));
   });
-  const body = { "id": shopIds[0], "url": shopUrls[0] };
+  $(".list-rst__rating-val").each((index: Number, element: String) => {
+    shopStars.push($(element).text());
+  });
+
+  // create response body
+  const body = { 
+    id: shopIds[0],
+    url: shopUrls[0],
+    star: shopStars[0],
+  };
+
+  // return http response
   return {
     statusCode: 200,
     headers: { "Access-Control-Allow-Origin": "*" },
--- test/taberogu.test.ts
+++ test/taberogu.test.ts
@@ -1,8 +1,19 @@
 import Taberogu = require('../taberogu/taberogu');
 
 test("taberogu", async () => {
-  const res = await Taberogu.get(null);
-
-  expect(res.statusCode).toBe(200); 
-  expect(res.body).toBe("success");
+  // request data
+  const event = {
+    queryStringParameters: {
+      name: "日高屋",
+    },
+  }
+  // call api
+  const res = await Taberogu.getShop(event);
+  const body = JSON.parse(res.body);
+  // check response
+  expect(res.statusCode).toBe(200);
+  expect(res.headers["Access-Control-Allow-Origin"]).toBe("*");
+  expect(body.id).toBe("11012499");
+  expect(body.url).toBe("https://tabelog.com/saitama/A1101/A110101/11012499/");
+  expect(body.star).toBe("3.05");
 });
