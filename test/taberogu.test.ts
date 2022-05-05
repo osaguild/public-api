d2f1473dea5b0b05243dfc52df18a4c341b381e9
nobuhisa.abe <33506754+osaguild@users.noreply.github.com>
2022-05-05 18:35:48 +0900

    v0.1.1 (#17)

--- test/taberogu.test.ts
+++ test/taberogu.test.ts
@@ -1,6 +1,6 @@
 import Taberogu = require('../taberogu/taberogu');
 
-test("taberogu", async () => {
+test("success", async () => {
   // request data
   const event = {
     queryStringParameters: {
@@ -16,4 +16,19 @@ test("taberogu", async () => {
   expect(body.id).toBe("11012499");
   expect(body.url).toBe("https://tabelog.com/saitama/A1101/A110101/11012499/");
   expect(body.star).toBe("3.05");
+});
+
+test("not found", async () => {
+  // request data
+  const event = {
+    queryStringParameters: {
+      name: "レストランラ・ヴォワール",
+    },
+  }
+  // call api
+  const res = await Taberogu.getShop(event);
+  // check response
+  expect(res.statusCode).toBe(404);
+  expect(res.headers["Access-Control-Allow-Origin"]).toBe("*");
+  expect(res.body).toBe("not found");
 });
