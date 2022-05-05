06a63b390671a50e219727127f1611fe7deb3982
osaguild <nobuhisa.abe.0922@gmail.com>
2022-05-05 21:26:08 +0900

    add

--- test/taberogu.test.ts
+++ test/taberogu.test.ts
@@ -4,7 +4,9 @@ test("success", async () => {
   // request data
   const event = {
     queryStringParameters: {
-      name: "日高屋",
+      prefecture: "saitama",
+      city: "saitama",
+      shopName: "日高屋",
     },
   }
   // call api
@@ -23,7 +25,9 @@ test("not found", async () => {
   // request data
   const event = {
     queryStringParameters: {
-      name: "レストランラ・ヴォワール",
+      prefecture: "saitama",
+      city: "saitama",
+      shopName: "レストランラ・ヴォワール",
     },
   }
   // call api
@@ -38,7 +42,9 @@ test("hit one", async () => {
   // request data
   const event = {
     queryStringParameters: {
-      name: "ひとり味",
+      prefecture: "saitama",
+      city: "saitama",
+      shopName: "ひとり味",
     },
   }
   // call api
@@ -53,11 +59,13 @@ test("hit one", async () => {
   expect(body.unique).toBe(true);
 });
 
-test("not found", async () => {
+test("request param error[prefecture]", async () => {
   // request data
   const event = {
     queryStringParameters: {
-      name: "レストランラ・ヴォワール",
+      prefecture: "tokyo",
+      city: "saitama",
+      shopName: "レストランラ・ヴォワール",
     },
   }
   // call api
@@ -65,5 +73,39 @@ test("not found", async () => {
   // check response
   expect(res.statusCode).toBe(404);
   expect(res.headers["Access-Control-Allow-Origin"]).toBe("*");
-  expect(res.body).toBe("not found");
+  expect(res.body).toBe("request params error. prefecture is not found");
+});
+
+test("request param error[city]", async () => {
+  // request data
+  const event = {
+    queryStringParameters: {
+      prefecture: "saitama",
+      city: "urawa",
+      shopName: "レストランラ・ヴォワール",
+    },
+  }
+  // call api
+  const res = await Taberogu.getShop(event);
+  // check response
+  expect(res.statusCode).toBe(404);
+  expect(res.headers["Access-Control-Allow-Origin"]).toBe("*");
+  expect(res.body).toBe("request params error. city is not found");
+});
+
+test("request param error[prefecture and city]", async () => {
+  // request data
+  const event = {
+    queryStringParameters: {
+      prefecture: "tokyo",
+      city: "urawa",
+      shopName: "レストランラ・ヴォワール",
+    },
+  }
+  // call api
+  const res = await Taberogu.getShop(event);
+  // check response
+  expect(res.statusCode).toBe(404);
+  expect(res.headers["Access-Control-Allow-Origin"]).toBe("*");
+  expect(res.body).toBe("request params error. prefecture is not found");
 });
