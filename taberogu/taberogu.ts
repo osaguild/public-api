294d67101fd695b9cfd3ab42fcfe116b5c6d5972
osaguild <nobuhisa.abe.0922@gmail.com>
2022-07-31 18:05:29 +0900

    refactoring taberogu.ts

--- taberogu/taberogu.ts
+++ taberogu/taberogu.ts
@@ -1,176 +1,174 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+import axios from "axios";
+import * as cheerio from "cheerio";
+import {
+  Prefecture,
+  GetShopRequest,
+  GetRankingRequest,
+  Response,
+  Shop,
+  Ranking,
+} from "./type";
 
-export async function getShop(event: any): Promise<any> {
-
-  // check request params
-  let prefectureCode;
-  let cityCode;
-
-  const searchableArea = {
-    prefectures: [{
-      name: "saitama",
-      code: "saitama",
-      cities: [{
+const searchableAreas: Prefecture[] = [
+  {
+    name: "saitama",
+    code: "saitama",
+    cities: [
+      {
         name: "saitama",
         code: "A1101",
-      }],
-    }],
-  };
+      },
+    ],
+  },
+];
 
-  for (const prefecture of searchableArea.prefectures) {
-    if (prefecture.name === event.queryStringParameters.prefecture) {
+const notFoundError = (message: string): Response => {
+  return {
+    statusCode: 404,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    body: message,
+  };
+};
+
+const unknownError = (): Response => {
+  return {
+    statusCode: 500,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    body: "unknown error occurs",
+  };
+};
+
+const getPrefectureAndCityCode = (prefectureName: string, cityName: string) => {
+  let prefectureCode: string | undefined = undefined;
+  let cityCode: string | undefined = undefined;
+  for (const prefecture of searchableAreas) {
+    if (prefecture.name === prefectureName) {
       prefectureCode = prefecture.code;
       for (const city of prefecture.cities) {
-        if (city.name === event.queryStringParameters.city) {
-          cityCode = city.code;
-        };
-      };
-    };
-  };
-
-  if (!prefectureCode) {
-    return {
-      statusCode: 404,
-      headers: { "Access-Control-Allow-Origin": "*" },
-      body: "request params error. prefecture is not found",
-    };
-  } else if (!cityCode) {
-    return {
-      statusCode: 404,
-      headers: { "Access-Control-Allow-Origin": "*" },
-      body: "request params error. city is not found",
-    };
+        if (city.name === cityName) cityCode = city.code;
+      }
+    }
   }
+  if (!prefectureCode)
+    throw new Error("request params error. prefecture is not found");
+  else if (!cityCode)
+    throw new Error("request params error. city is not found");
 
-  // api call
-  const uri = encodeURI(`https://tabelog.com/${prefectureCode}/${cityCode}/rstLst/?vs=1&sw=${event.queryStringParameters.shopName}`);
-  const res = await axios.get(uri);
+  return { prefectureCode, cityCode };
+};
 
-  // search dom
-  const $ = cheerio.load(res.data);
-  const shopIds: string[] = [];
-  const shopUrls: string[] = [];
-  const shopStars: string[] = [];
-  $(".js-rst-cassette-wrap").each((index: number, element: string) => {
-    shopIds.push($(element).attr("data-rst-id"));
-    shopUrls.push($(element).attr("data-detail-url"));
-  });
-  $(".list-rst__rating-val").each((index: null, element: string) => {
-    shopStars.push($(element).text());
-  });
-
-  // create response body
-  const body = {
-    id: shopIds[0],
-    url: shopUrls[0],
-    star: shopStars[0],
-    unique: shopIds.length === 1 ? true : false,
+export const getShop = async (request: GetShopRequest) => {
+  const scraping = async (
+    prefectureCode: string,
+    cityCode: string,
+    shopName: string
+  ) => {
+    // api call
+    const uri = encodeURI(
+      `https://tabelog.com/${prefectureCode}/${cityCode}/rstLst/?vs=1&sw=${shopName}`
+    );
+    const shopIds: string[] = [];
+    const shopUrls: string[] = [];
+    const shopStars: string[] = [];
+    const res = await axios.get(uri);
+    // search dom
+    const $ = cheerio.load(res.data);
+    $(".js-rst-cassette-wrap").each((i, e) => {
+      shopIds.push($(e).attr("data-rst-id") as string);
+      shopUrls.push($(e).attr("data-detail-url") as string);
+    });
+    $(".list-rst__rating-val").each((i, e) => {
+      shopStars.push($(e).text());
+    });
+    if (shopIds.length === 0 || shopUrls.length === 0 || shopStars.length === 0)
+      throw new Error("not found");
+    return {
+      id: shopIds[0],
+      url: shopUrls[0],
+      star: shopStars[0],
+      unique: shopIds.length === 1 ? true : false,
+    } as Shop;
   };
 
-  // return http response
-  if (shopIds.length === 0) {
-    return {
-      statusCode: 404,
-      headers: { "Access-Control-Allow-Origin": "*" },
-      body: "not found",
-    };
-  } else {
+  try {
+    // check request params
+    const { prefectureCode, cityCode } = getPrefectureAndCityCode(
+      request.queryStringParameters.prefecture,
+      request.queryStringParameters.city
+    );
+
+    // scraping
+    const shop = await scraping(
+      prefectureCode,
+      cityCode,
+      request.queryStringParameters.shopName
+    );
+
+    // return response
     return {
       statusCode: 200,
       headers: { "Access-Control-Allow-Origin": "*" },
-      body: JSON.stringify(body),
-    };
+      body: JSON.stringify(shop),
+    } as Response;
+  } catch (e) {
+    if (e instanceof Error) return notFoundError(e.message);
+    else return unknownError();
   }
 };
 
-export async function getRanking(event: any): Promise<any> {
+export const getRanking = async (request: GetRankingRequest) => {
+  const scraping = async (prefectureCode: string, cityCode: string) => {
+    const shopIds: string[] = [];
+    const shopUrls: string[] = [];
+    const shopStars: string[] = [];
+    const shopRanking: string[] = [];
+    // get ranking every 20
+    for (let page = 1; page <= 5; page++) {
+      // api call
+      const uri = encodeURI(
+        `https://tabelog.com/${prefectureCode}/${cityCode}/rstLst/${page}/?Srt=D&SrtT=rt&sort_mode=1&select_sort_flg=1`
+      );
+      const res = await axios.get(uri);
 
-  // check request params
-  let prefectureCode;
-  let cityCode;
-
-  const searchableArea = {
-    prefectures: [{
-      name: "saitama",
-      code: "saitama",
-      cities: [{
-        name: "saitama",
-        code: "A1101",
-      }],
-    }],
-  };
-
-  for (const prefecture of searchableArea.prefectures) {
-    if (prefecture.name === event.queryStringParameters.prefecture) {
-      prefectureCode = prefecture.code;
-      for (const city of prefecture.cities) {
-        if (city.name === event.queryStringParameters.city) {
-          cityCode = city.code;
-        };
-      };
-    };
-  };
-
-  if (!prefectureCode) {
-    return {
-      statusCode: 404,
-      headers: { "Access-Control-Allow-Origin": "*" },
-      body: "request params error. prefecture is not found",
-    };
-  } else if (!cityCode) {
-    return {
-      statusCode: 404,
-      headers: { "Access-Control-Allow-Origin": "*" },
-      body: "request params error. city is not found",
-    };
-  }
-
-  // api call
-  const shopIds: string[] = [];
-  const shopUrls: string[] = [];
-  const shopStars: string[] = [];
-  const shopRanking: string[] = [];
-
-  for (let page = 1; page <= 5; page++) {
-    const uri = encodeURI(`https://tabelog.com/${prefectureCode}/${cityCode}/rstLst/${page}/?Srt=D&SrtT=rt&sort_mode=1&select_sort_flg=1`);
-    const res = await axios.get(uri);
-
-    // search dom
-    const $ = cheerio.load(res.data);
-    $(".js-rst-cassette-wrap").each((index: number, element: string) => {
-      shopIds.push($(element).attr("data-rst-id"));
-      shopUrls.push($(element).attr("data-detail-url"));
-      shopRanking.push(((page - 1) * 20 + index + 1).toString());
-    });
-    $(".list-rst__rating-val").each((index: number, element: string) => {
-      shopStars.push($(element).text());
+      // search dom
+      const $ = cheerio.load(res.data);
+      $(".js-rst-cassette-wrap").each((i, e) => {
+        shopIds.push($(e).attr("data-rst-id") as string);
+        shopUrls.push($(e).attr("data-detail-url") as string);
+        shopRanking.push(((page - 1) * 20 + i + 1).toString());
+      });
+      $(".list-rst__rating-val").each((i, e) => {
+        shopStars.push($(e).text());
+      });
+    }
+    return shopIds.map((e, i) => {
+      return {
+        id: shopIds[i],
+        url: shopUrls[i],
+        star: shopStars[i],
+        ranking: shopRanking[i],
+      } as Ranking;
     });
   };
 
-  // create response body
-  const body: Object[] = [];
-  for (let i = 0; i < shopIds.length; i++) {
-    body.push({
-      id: shopIds[i],
-      url: shopUrls[i],
-      star: shopStars[i],
-      ranking: shopRanking[i],
-    });
-  };
+  try {
+    // check request params
+    const { prefectureCode, cityCode } = getPrefectureAndCityCode(
+      request.queryStringParameters.prefecture,
+      request.queryStringParameters.city
+    );
 
-  // return http response
-  if (shopIds.length === 0) {
-    return {
-      statusCode: 404,
-      headers: { "Access-Control-Allow-Origin": "*" },
-      body: "not found",
-    };
-  } else {
+    // scraping
+    const ranking = await scraping(prefectureCode, cityCode);
+
+    // return response
     return {
       statusCode: 200,
       headers: { "Access-Control-Allow-Origin": "*" },
-      body: JSON.stringify(body),
-    };
+      body: JSON.stringify(ranking),
+    } as Response;
+  } catch (e) {
+    if (e instanceof Error) return notFoundError(e.message);
+    else return unknownError();
   }
-}
+};
--- test/taberogu.test.ts
+++ test/taberogu.test.ts
@@ -1,133 +1,162 @@
-import Taberogu = require("../taberogu/taberogu");
+import { getShop, getRanking } from "../taberogu/taberogu";
+import {
+  GetShopRequest,
+  GetRankingRequest,
+  Shop,
+  Ranking,
+} from "../taberogu/type";
 
-jest.setTimeout(20000);
+jest.setTimeout(60000);
 
-test("success", async () => {
-  // request data
-  const event = {
-    queryStringParameters: {
-      prefecture: "saitama",
-      city: "saitama",
-      shopName: "酒蔵 力",
-    },
-  };
-  // call api
-  const res = await Taberogu.getShop(event);
-  const body = JSON.parse(res.body);
-  // check response
-  expect(res.statusCode).toBe(200);
-  expect(res.headers["Access-Control-Allow-Origin"]).toBe("*");
-  expect(body.id).toBe("11000484");
-  expect(body.url).toBe("https://tabelog.com/saitama/A1101/A110102/11000484/");
-  expect(Number(body.star)).toBeGreaterThan(3.3);
-  expect(body.unique).toBe(false);
+describe("test for getShop()", () => {
+  it("[success]hit multiple shops", async () => {
+    // request
+    const req: GetShopRequest = {
+      queryStringParameters: {
+        prefecture: "saitama",
+        city: "saitama",
+        shopName: "酒蔵 力",
+      },
+    };
+    // call api
+    const res = await getShop(req);
+    const body: Shop = JSON.parse(res.body);
+    // check response
+    expect(res.statusCode).toBe(200);
+    expect(res.headers["Access-Control-Allow-Origin"]).toBe("*");
+    expect(body.id).toBe("11000484");
+    expect(body.url).toBe(
+      "https://tabelog.com/saitama/A1101/A110102/11000484/"
+    );
+    expect(Number(body.star)).toBeGreaterThan(3.3);
+    expect(body.unique).toBe(false);
+  });
+
+  it("[success]hit one shop", async () => {
+    // request data
+    const req: GetShopRequest = {
+      queryStringParameters: {
+        prefecture: "saitama",
+        city: "saitama",
+        shopName: "ひとり味",
+      },
+    };
+    // call api
+    const res = await getShop(req);
+    const body: Shop = JSON.parse(res.body);
+    // check response
+    expect(res.statusCode).toBe(200);
+    expect(res.headers["Access-Control-Allow-Origin"]).toBe("*");
+    expect(body.id).toBe("11038373");
+    expect(body.url).toBe(
+      "https://tabelog.com/saitama/A1101/A110102/11038373/"
+    );
+    expect(Number(body.star)).toBeGreaterThan(3);
+    expect(body.unique).toBe(true);
+  });
+
+  it("[failed]not found error", async () => {
+    // request data
+    const req: GetShopRequest = {
+      queryStringParameters: {
+        prefecture: "saitama",
+        city: "saitama",
+        shopName: "レストランラ・ヴォワール",
+      },
+    };
+    // call api
+    const res = await getShop(req);
+    // check response
+    expect(res.statusCode).toBe(404);
+    expect(res.headers["Access-Control-Allow-Origin"]).toBe("*");
+    expect(res.body).toBe("not found");
+  });
+
+  it("[failed]request param error[prefecture]", async () => {
+    // request data
+    const req: GetShopRequest = {
+      queryStringParameters: {
+        prefecture: "tokyo",
+        city: "saitama",
+        shopName: "レストランラ・ヴォワール",
+      },
+    };
+    // call api
+    const res = await getShop(req);
+    // check response
+    expect(res.statusCode).toBe(404);
+    expect(res.headers["Access-Control-Allow-Origin"]).toBe("*");
+    expect(res.body).toBe("request params error. prefecture is not found");
+  });
+
+  it("[failed]request param error[city]", async () => {
+    // request data
+    const req: GetShopRequest = {
+      queryStringParameters: {
+        prefecture: "saitama",
+        city: "urawa",
+        shopName: "レストランラ・ヴォワール",
+      },
+    };
+    // call api
+    const res = await getShop(req);
+    // check response
+    expect(res.statusCode).toBe(404);
+    expect(res.headers["Access-Control-Allow-Origin"]).toBe("*");
+    expect(res.body).toBe("request params error. city is not found");
+  });
 });
 
-test("not found", async () => {
-  // request data
-  const event = {
-    queryStringParameters: {
-      prefecture: "saitama",
-      city: "saitama",
-      shopName: "レストランラ・ヴォワール",
-    },
-  };
-  // call api
-  const res = await Taberogu.getShop(event);
-  // check response
-  expect(res.statusCode).toBe(404);
-  expect(res.headers["Access-Control-Allow-Origin"]).toBe("*");
-  expect(res.body).toBe("not found");
-});
+describe("test for getRanking()", () => {
+  it("[success]ranking", async () => {
+    // request data
+    const req: GetRankingRequest = {
+      queryStringParameters: {
+        prefecture: "saitama",
+        city: "saitama",
+      },
+    };
+    // call api
+    const res = await getRanking(req);
+    const body: Ranking[] = JSON.parse(res.body);
+    // check response
+    expect(res.statusCode).toBe(200);
+    expect(res.headers["Access-Control-Allow-Origin"]).toBe("*");
+    expect(Number(body[0].star)).toBeGreaterThan(3.5);
+    expect(body[0].ranking).toBe("1");
+    expect(Number(body[99].star)).toBeGreaterThan(3.5);
+    expect(body[99].ranking).toBe("100");
+  });
 
-test("hit one", async () => {
-  // request data
-  const event = {
-    queryStringParameters: {
-      prefecture: "saitama",
-      city: "saitama",
-      shopName: "ひとり味",
-    },
-  };
-  // call api
-  const res = await Taberogu.getShop(event);
-  const body = JSON.parse(res.body);
-  // check response
-  expect(res.statusCode).toBe(200);
-  expect(res.headers["Access-Control-Allow-Origin"]).toBe("*");
-  expect(body.id).toBe("11038373");
-  expect(body.url).toBe("https://tabelog.com/saitama/A1101/A110102/11038373/");
-  expect(Number(body.star)).toBeGreaterThan(3);
-  expect(body.unique).toBe(true);
-});
+  it("[failed]request param error[prefecture]", async () => {
+    // request data
+    const req: GetRankingRequest = {
+      queryStringParameters: {
+        prefecture: "tokyo",
+        city: "saitama",
+      },
+    };
+    // call api
+    const res = await getRanking(req);
+    // check response
+    expect(res.statusCode).toBe(404);
+    expect(res.headers["Access-Control-Allow-Origin"]).toBe("*");
+    expect(res.body).toBe("request params error. prefecture is not found");
+  });
 
-test("request param error[prefecture]", async () => {
-  // request data
-  const event = {
-    queryStringParameters: {
-      prefecture: "tokyo",
-      city: "saitama",
-      shopName: "レストランラ・ヴォワール",
-    },
-  };
-  // call api
-  const res = await Taberogu.getShop(event);
-  // check response
-  expect(res.statusCode).toBe(404);
-  expect(res.headers["Access-Control-Allow-Origin"]).toBe("*");
-  expect(res.body).toBe("request params error. prefecture is not found");
-});
-
-test("request param error[city]", async () => {
-  // request data
-  const event = {
-    queryStringParameters: {
-      prefecture: "saitama",
-      city: "urawa",
-      shopName: "レストランラ・ヴォワール",
-    },
-  };
-  // call api
-  const res = await Taberogu.getShop(event);
-  // check response
-  expect(res.statusCode).toBe(404);
-  expect(res.headers["Access-Control-Allow-Origin"]).toBe("*");
-  expect(res.body).toBe("request params error. city is not found");
-});
-
-test("request param error[prefecture and city]", async () => {
-  // request data
-  const event = {
-    queryStringParameters: {
-      prefecture: "tokyo",
-      city: "urawa",
-      shopName: "レストランラ・ヴォワール",
-    },
-  };
-  // call api
-  const res = await Taberogu.getShop(event);
-  // check response
-  expect(res.statusCode).toBe(404);
-  expect(res.headers["Access-Control-Allow-Origin"]).toBe("*");
-  expect(res.body).toBe("request params error. prefecture is not found");
-});
-
-test("ranking", async () => {
-  // request data
-  const event = {
-    queryStringParameters: {
-      prefecture: "saitama",
-      city: "saitama",
-    },
-  };
-  // call api
-  const res = await Taberogu.getRanking(event);
-  const body = JSON.parse(res.body);
-  // check response
-  expect(res.statusCode).toBe(200);
-  expect(res.headers["Access-Control-Allow-Origin"]).toBe("*");
-  expect(Number(body[0].star)).toBeGreaterThan(3.5);
-  expect(body[0].ranking).toBe("1");
-  expect(Number(body[99].star)).toBeGreaterThan(3.5);
-  expect(body[99].ranking).toBe("100");
+  it("[failed]request param error[city]", async () => {
+    // request data
+    const req: GetRankingRequest = {
+      queryStringParameters: {
+        prefecture: "saitama",
+        city: "urawa",
+      },
+    };
+    // call api
+    const res = await getRanking(req);
+    // check response
+    expect(res.statusCode).toBe(404);
+    expect(res.headers["Access-Control-Allow-Origin"]).toBe("*");
+    expect(res.body).toBe("request params error. city is not found");
+  });
 });
