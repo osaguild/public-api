c494875df7c6419c170c1cf1a5a2f5264641c2e5
osaguild <nobuhisa.abe.0922@gmail.com>
2022-08-13 09:15:22 +0900

    add limit of building

--- src/shamaison/index.ts
+++ src/shamaison/index.ts
@@ -15,25 +15,27 @@ export const sendShamaisonMessage = async () => {
     const title = `🎉${date.getFullYear()}年${
       date.getMonth() + 1
     }月${date.getDate()}日 ${stations
-      .map((e) => `${e.name}/`)
-      .slice(0, -1)}の物件情報🎉\n`;
-
+      .map((e) => `${e.name}`)
+      .join("/")}の物件情報🎉\n`;
     // e.g: 【シャーメゾン】JR山手線 新宿駅 徒歩10分 https://www.shamaison.com/tokyo/area/00000/00000000/
+    // get 100 buildings because of the limit of the message
     const buildingInfo =
       buildings.length === 0
         ? "対象地域の物件情報はありません\n"
         : buildings
+            .slice(0, 50)
             .map((e) => {
               return `【${e.name}】\n${e.station} ${e.distance}\n${e.url}\n`;
             })
             .join("\n");
-
+    // e.g: ※文字数制限のため上限50件として配信しています。
+    const warn = "※文字数制限のため上限50件として配信しています。\n";
     // e.g: ⭐カルディ公式サイト⭐https://www.shamaison.com/tokyo/route/0000000/station/00000
-    const officialLink = `⭐シャーメゾン公式サイト⭐\n${stations.map(
-      (e) => `${e.name}: https://www.shamaison.com${e.url}\n`
-    )}`;
+    const officialLink = `⭐シャーメゾン公式サイト⭐\n${stations
+      .map((e) => `${e.name}: https://www.shamaison.com${e.url}`)
+      .join("\n")}`;
 
-    return `${title}\n${buildingInfo}\n${officialLink}`;
+    return `${title}\n${buildingInfo}\n${warn}\n${officialLink}`;
   };
 
   try {
